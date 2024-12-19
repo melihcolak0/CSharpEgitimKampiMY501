05a25c93ba0110a005cb9c04a0b71ce76c7697a0
Melih Çolak <melihcolak0@gmail.com>
2024-12-19 12:43:52 +0300

    Add project files.

---
+++ CSharpEgitimKampiMY501/Repositories/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using CSharpEgitimKampiMY501.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampiMY501.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<List<ResultProductDto>> GetAllProductAsync();
+        Task CreateProductAsync(CreateProductDto createProductDto);
+        Task UpdateProductAsync(UpdateProductDto updateProductDto);
+        Task DeleteProductAsync(int id);
+        Task GetByProductIdAsync(int id);
+    }
+}
